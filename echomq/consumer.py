470ccf581b7e71f72ec721a7e97696961d3dd8e0
Mher Movsisyan <mher.movsisyan@gmail.com>
2012-10-16 21:10:36 +0400

    Adds non-blocking, Tornado ioloop based consumer

--- echomq/consumer.py
+++ echomq/consumer.py
@@ -1,6 +1,7 @@
 import logging
 import threading
 
+from tornado import ioloop
 from kombu import Connection, Exchange, Queue
 
 
@@ -30,3 +31,29 @@ class ConsumerThread(threading.Thread):
                 logging.info('Connected to: %s' % conn.as_uri())
                 while True:
                     conn.drain_events()
+
+
+class TornadoConsumer(object):
+    """Non-blocking, Tornado ioloop based consumer"""
+
+    def __init__(self, broker_url, exchange, exchange_type, queue,
+                       routing_key, durable=True, io_loop=None):
+        self.io_loop = io_loop or ioloop.IoLoop.instance()
+        self._conn = None
+        self._callbacks = []
+        self._broker_url = broker_url
+
+        self._exchange = Exchange(exchange, exchange_type, durable=durable)
+        self._queue = Queue(queue, exchange=self._exchange, routing_key=routing_key)
+
+    def add_callback(self, callback):
+        assert not self._conn
+        self._callbacks.append(callback)
+
+    def start(self):
+        self._conn = Connection(self._broker_url)
+        self._consumer = self._conn.Consumer(self._queue, callbacks=self._callbacks)
+        self.io_loop.add_handler(self._conn.fileno(), self._handle_event)
+
+    def _handle_event(self):
+        self._conn.drain_nowait()
